6e3b356b5d423c69422d8963ea2ad0998aec9bdb
Matej Lesko <lesko.matej.pu@gmail.com>
2021-02-03 00:44:43 +0100

    cheeses app creation

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,6 +85,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "everycheese.users.apps.UsersConfig",
+    "everycheese.cheeses.apps.CheesesConfig"
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
@@ -93,9 +94,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 # MIGRATIONS
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#migration-modules
-MIGRATION_MODULES = {
-    "sites": "everycheese.contrib.sites.migrations"
-}
+MIGRATION_MODULES = {"sites": "everycheese.contrib.sites.migrations"}
 
 # AUTHENTICATION
 # ------------------------------------------------------------------------------
@@ -126,15 +125,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 # MIDDLEWARE
@@ -273,9 +266,7 @@ LOGGING = {
 
 # django-allauth
 # ------------------------------------------------------------------------------
-ACCOUNT_ALLOW_REGISTRATION = env.bool(
-    "DJANGO_ACCOUNT_ALLOW_REGISTRATION", True
-)
+ACCOUNT_ALLOW_REGISTRATION = env.bool("DJANGO_ACCOUNT_ALLOW_REGISTRATION", True)
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_AUTHENTICATION_METHOD = "username"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
@@ -285,9 +276,7 @@ ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_ADAPTER = "everycheese.users.adapters.AccountAdapter"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-SOCIALACCOUNT_ADAPTER = (
-    "everycheese.users.adapters.SocialAccountAdapter"
-)
+SOCIALACCOUNT_ADAPTER = "everycheese.users.adapters.SocialAccountAdapter"
 
 # Your stuff...
 # ------------------------------------------------------------------------------
---
+++ everycheese/cheeses/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+from model_utils.models import TimeStampedModel
+from autoslug import AutoSlugField
+
+
+class Cheese(TimeStampedModel):
+    def __str__(self) -> str:
+        return "%s %s %s" % (self.name, self.slug, self.firmness)
+
+    class Firmness(models.TextChoices):
+        UNSPECIFIED = "unspecified", "Unspecified"
+        SOFT = "soft", "Soft"
+        SEMI_SOFT = "semi-soft", "Semi-Soft"
+        SEMI_HARD = "semi-hard", "Semi-Hard"
+        HARD = "hard", "Hard"
+
+    name = models.CharField("name of the Cheese", max_length=255)
+    slug = AutoSlugField(
+        "Cheese Address", unique=True, always_update=False, populate_from="name"
+    )
+    description = models.TextField("Description", blank=True)
+    firmness = models.CharField(
+        "Firmness",
+        max_length=20,
+        default=Firmness.UNSPECIFIED,
+        choices=Firmness.choices,
+    )
