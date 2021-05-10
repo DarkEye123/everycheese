85f4880be694580cd093a508381862d816e56f2b
Matej Lesko <lesko.matej.pu@gmail.com>
2021-05-10 12:19:37 +0200

    cheese creation accessible only to logged users

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 from .models import Cheese
@@ -12,7 +13,7 @@ class CheesesDetailView(DetailView):
     model = Cheese
 
 
-class CheeseCreateView(CreateView):
+class CheeseCreateView(LoginRequiredMixin, CreateView):
     model = Cheese
 
     fields = ["name", "description", "firmness", "country"]
