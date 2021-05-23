f7714dbdd75aa959ec26edcc5d0bccce52e92305
Matej Lesko <lesko.matej.pu@gmail.com>
2021-05-23 18:01:30 +0200

    fill creator field automatically

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -1,3 +1,5 @@
+from django.forms.forms import BaseForm
+from django.http.response import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -17,3 +19,7 @@ class CheeseCreateView(LoginRequiredMixin, CreateView):
     model = Cheese
 
     fields = ["name", "description", "firmness", "country"]
+
+    def form_valid(self, form: BaseForm) -> HttpResponse:
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
